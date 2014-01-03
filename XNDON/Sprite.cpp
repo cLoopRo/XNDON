a4bfef40d4a1b50faa63af898d749734364cf9c7
worldweary <adtbundle@gmail.com>
2014-01-04 00:13:03 +0900

    name Change Coord to Vector and pos to Position and vel to veloticy

--- XNDON/Sprite.cpp
+++ XNDON/Sprite.cpp
@@ -1,8 +1,117 @@
 #include "Sprite.h"
 
 
-Sprite :: ~Sprite(void)
-{
-
-
+Sprite::Sprite( ) {
+	is_controll = 1;
+	max_hp = 1;
+	hp = 1;
+	state = 0; //??
+	position = Vector3();
+	velocity = Vector3();
 }
+
+Sprite::Sprite(int _MaxHP, int _HP, const Vector3& _Position, const Vector3& _Velocity) {
+	direction = true;
+	is_controll = 1;
+	max_hp = _MaxHP;
+	hp = _HP;
+	state = 0; //??
+	position.X = _Position.X;
+	position.Y = _Position.Y;
+	position.Z = _Position.Z;
+	velocity.X = _Velocity.X;
+	velocity.Y = _Velocity.Y;
+	velocity.Z = _Velocity.Z;
+}
+
+Sprite::Sprite(int _Controll, int _MaxHP, int _HP, const Vector3& _Position, const Vector3& _Velocity) {
+	direction = true;
+	is_controll = _Controll;
+	max_hp = _MaxHP;
+	hp = _HP;
+	state = 0; //??
+	position.X = _Position.X;
+	position.Y = _Position.Y;
+	position.Z = _Position.Z;
+	velocity.X = _Velocity.X;
+	velocity.Y = _Velocity.Y;
+	velocity.Z = _Velocity.Z;
+}
+
+Sprite::Sprite(int _MaxHP, int _HP, int _PosX, int _PosY, int _PosZ, int _VelX, int _VelY, int _VelZ) {
+	direction = true;
+	is_controll = 1;
+	max_hp = _MaxHP;
+	hp = _HP;
+	state = 0; //??
+	position = Vector3(_PosX, _PosY, _PosZ);
+	velocity = Vector3(_VelZ, _VelY, _VelZ);
+}
+
+Sprite::Sprite(int _Controll, int _MaxHP, int _HP, int _PosX, int _PosY, int _PosZ, int _VelX, int _VelY, int _VelZ) {
+	direction = true;
+	is_controll = _Controll;
+	max_hp = _MaxHP;
+	hp = _HP;
+	state = 0; //??
+	position = Vector3(_PosX, _PosY, _PosZ);
+	velocity = Vector3(_VelZ, _VelY, _VelZ);
+}
+
+
+
+Sprite::Sprite(bool _Direction, int _MaxHP, int _HP, const Vector3& _Position, const Vector3& _Velocity) {
+	direction = _Direction;
+	is_controll = 1;
+	max_hp = _MaxHP;
+	hp = _HP;
+	state = 0; //??
+	position.X = _Position.X;
+	position.Y = _Position.Y;
+	position.Z = _Position.Z;
+	velocity.X = _Velocity.X;
+	velocity.Y = _Velocity.Y;
+	velocity.Z = _Velocity.Z;
+}
+
+Sprite::Sprite(bool _Direction, int _Controll, int _MaxHP, int _HP, const Vector3& _Position, const Vector3& _Velocity) {
+	direction = _Direction;
+	is_controll = _Controll;
+	max_hp = _MaxHP;
+	hp = _HP;
+	state = 0; //??
+	position.X = _Position.X;
+	position.Y = _Position.Y;
+	position.Z = _Position.Z;
+	velocity.X = _Velocity.X;
+	velocity.Y = _Velocity.Y;
+	velocity.Z = _Velocity.Z;
+}
+
+Sprite::Sprite(bool _Direction, int _MaxHP, int _HP, int _PosX, int _PosY, int _PosZ, int _VelX, int _VelY, int _VelZ) {
+	direction = _Direction;
+	is_controll = 1;
+	max_hp = _MaxHP;
+	hp = _HP;
+	state = 0; //??
+	position = Vector3(_PosX, _PosY, _PosZ);
+	velocity = Vector3(_VelZ, _VelY, _VelZ);
+}
+
+Sprite::Sprite(bool _Direction, int _Controll, int _MaxHP, int _HP, int _PosX, int _PosY, int _PosZ, int _VelX, int _VelY, int _VelZ) {
+	direction = _Direction;
+	is_controll = _Controll;
+	max_hp = _MaxHP;
+	hp = _HP;
+	state = 0; //??
+	position = Vector3(_PosX, _PosY, _PosZ);
+	velocity = Vector3(_VelZ, _VelY, _VelZ);
+}
+
+
+
+Sprite::~Sprite(void)
+{
+	//..?
+}
+
