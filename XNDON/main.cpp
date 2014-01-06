a2f25ca89b53a228c438c1b4476eccf8c39e3cc3
cLoopRo <cLoopRo@gmail.com>
2014-01-06 17:35:47 +0900

    Add comment about WIN API

--- XNDON/main.cpp
+++ XNDON/main.cpp
@@ -2,12 +2,19 @@
 #include "Render.h"
 #include "InputController.h"
 #include "ResManager.h"
-#include <ctime>
 
-LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
-HINSTANCE g_hInst;
-HWND hWndMain;
-LPCTSTR lpszClass=TEXT("XNDON");
+#include <tchar.h>
+
+
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM); 
+HINSTANCE g_hInst; // 핸들 인스턴스 , H 로 시작하는 자료형은 다 핸들
+
+/*<--- 프로그램 윈도우 타이틀 정의 -->*/
+LPCTSTR lpszClass = TEXT("XNDON"); // TEXT("문자") => L"문자" => _T("문자");
+
+HWND hWndMain; // HWND : 윈도우 핸들을 담는 전용 타입, 유사) HANDLE : 범용핸들 타입
+// hWND 가 전역(static)일 시 제대로 작동 안함
+
 
 Scene* Render :: pScene = NULL;
 vector<Sprite*> Render :: sceneObject;
@@ -15,20 +22,26 @@ int Render :: dTime = NULL;
 
 CachedBitmap* Render :: pCBit = NULL;
 bool InputController :: keys[256];
-
-
 std::map<wstring, Image* > ResManager :: image_map;
 
 void OnPaint(HDC hdc);		
 int lastTime = 0;
 int dt = 0;
+
+
+
+
+
+// 우리가 Console 에서 사용하던 int main 과 유사한, 윈도우 창을 만드는 winMain
+// winMain 기본인자는 아래와 같다.
+// WinMain 으로 프로그램을 시작하며 실질적인 처리는 WndProc 함수에서 한다.
 int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance
-       ,LPSTR lpszCmdParam,int nCmdShow)
+       ,LPSTR lpszCmdParam,int nCmdShow) 
 {
 	HWND hWnd;
     MSG Message;
-    WNDCLASS WndClass;
-    g_hInst=hInstance;
+    
+    g_hInst = hInstance;
 	
 	/*<----- GdiPlus 시작 ----->*/
 	ULONG_PTR gpToken;
@@ -40,21 +53,27 @@ int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance
 	} 
 	/*<------------------------>*/
  
-	
-    WndClass.cbClsExtra=0;
-    WndClass.cbWndExtra=0;
-    WndClass.hbrBackground=(HBRUSH)(COLOR_WINDOW+1);
-	WndClass.hCursor=LoadCursor(NULL,IDC_ARROW);
-    WndClass.hIcon=LoadIcon(NULL,IDI_APPLICATION);
-	WndClass.hInstance=hInstance;
-	WndClass.lpfnWndProc=WndProc;
-	WndClass.lpszClassName=lpszClass;
-	WndClass.lpszMenuName=NULL;
-	WndClass.style=CS_HREDRAW | CS_VREDRAW;
-	RegisterClass(&WndClass);
- 
-	hWnd=CreateWindow(lpszClass,lpszClass,WS_OVERLAPPEDWINDOW, 
-		0 , 0, 1280, 720, NULL,(HMENU)NULL,hInstance,NULL);
+
+	/*<--- 윈도우( 입력과 출력을 할 수 있도록 해준다.) 클래스 --->*/
+    WNDCLASS WndClass;
+
+	WndClass.cbClsExtra=0;
+    WndClass.cbWndExtra=0; 
+    WndClass.hbrBackground=(HBRUSH)(COLOR_WINDOW+1); //윈도우 배경색상 지정
+	WndClass.hCursor = LoadCursor(NULL,IDC_ARROW); // 마우스 커서
+    WndClass.hIcon = LoadIcon(NULL,IDI_APPLICATION); // 최소화시 출력 아이콘
+	WndClass.hInstance = hInstance; // 윈도우 클래스를 사용하는 프로그램의 번호, WinMain의 인자값 사용
+	WndClass.lpfnWndProc = WndProc; // 윈도우의 메세지처리함수 지정, 관습적으로 이름 WndProc 사용
+	WndClass.lpszClassName = lpszClass; // 윈도우 클래스의 이름 정의 : 윈도우의 이름
+	WndClass.lpszMenuName=NULL; // 프로그램이 사용할 메뉴 지정, 코드로 만드는 것이 아닌 리소스 에디터에 의해 만들어 진 후 링크시 합쳐지도록. NULL 은 사용하지 않음
+	WndClass.style=CS_HREDRAW | CS_VREDRAW; // 윈도우가 수직, 수평으로 크기가 변할 경우 다시 그린다.
+	RegisterClass(&WndClass); // 윈도우 클래스 등록
+
+	// 윈도우 생성
+	hWnd = CreateWindow(lpszClass,lpszClass,WS_OVERLAPPEDWINDOW, 
+		0 , 0, CL_WIN_WIDTH_PIXEL, CL_WIN_HEIGHT_PIXEL, NULL,(HMENU)NULL,hInstance,NULL);
+	/*<--- 윈도우 클래스 등록 및 생성 완료 --->*/
+
 	
 	ShowWindow(hWnd,nCmdShow);
  
--- XNDON/main.h
+++ XNDON/main.h
@@ -24,6 +24,8 @@ using std::queue;
 #include <string>
 using std::wstring;
 
+#include <ctime>
+//#include <tchar.h>
 
 class Vector3{
 public:
@@ -37,3 +39,57 @@ public:
 };
 
 static bool keys[256];	// Array Used For The Keyboard Routine
+
+
+int const CL_WIN_WIDTH_PIXEL = 800; 
+int const CL_WIN_HEIGHT_PIXEL = 640;
+
+/*
+
+PWSTR  lpszStr = L("My string"); // 유니코트 문자열로 지정하는 매크로 함수 L()
+'W' => 유니코드
+
+TCHAR : Windows시스템에서 유니코드와 MBCS(Multi Byte Character Set)을 지원하기 위해 TCHAR을 사용
+TCHAR 을 나타는 매크로 _T : (한문자 _T(' '), 문장 _T(" "))
+
+MultiByte 는 어떤 글씨는 1바이트 이고 어떤 글씨는 2바이트여서 메모리 관리가 어렵다.
+그래서 나온 것이 wchar 인데, wchar 는 모든 글씨(영문 포함)가 2바이트로 구성이 된다.
+당연히 wchar 는 일반 ascii 타입의 char 보다 메모리 공간이 2배 필요하다.
+
+TCHAR 는 ACSCII 타입의 일반 char 또는 wchar 라는 의미다.
+TCHAR 을 사용하면 char 인지 wchar 인지 구별하지 않고 그냥 코딩을 하면 된다.
+
+CHAR * == LPSTR == PSTR
+CONST CHAR * == LPCSTR == PCSTR
+
+LPTSTR  = TCHAR *
+LPCTSTR = CONST TCHAR *
+
+'C' => const 의 의미, 'LP' => long pointer 의 의미.
+'STR' => NULL 포인터로 끝난다의 의미
+
+
+ANSI            Unicode          TCHAR type
+--------------------------------------
+strlen()        wcslen()         _tcslen()
+strcat()        wcscat()         _tcscat()
+strchr()        wcschr()         _tcschr()
+strcmp()      wcscmp()       _tcscmp()
+strcpy()       wcscpy()        _tcscpy()
+strstr()         wcsstr()          _tcsstr()
+strrev()        _wcsrev()        _tcsrev()
+
+printf()         wprintf()           _tprintf()
+sprintf()       swprintf()         _stprintf()
+scanf()        wscanf()          _tscanf()
+=> tchar.h 에 정의되어 있음 
+
+
+BYTE	unsigned char형
+WORD	unsigned short형
+DWORD	unsigned long(int)형
+
+
+
+*/
+
