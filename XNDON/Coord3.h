d3e839346b61a43be172b21718ae7b594686a2d5
unknown <public2@PoolC-Dalmatia0.poolc.org>
2014-01-06 21:42:00 +0900

    fix XDCube.cpp

--- XNDON/Coord3.h
+++ XNDON/Coord3.h
@@ -2,11 +2,11 @@
 
 class Coord3{
 public:
-	Coord3( ){	};
-	Coord3(double _X, double _Y, double _Z) : X(_X), Y(_Y), Z(_Z)
-	{	}; 
+	Coord3( ){	}
+	Coord3(double _X, double _Y, double _Z) : X(_X), Y(_Y), Z(_Z) {	}
 		
 	double X;
 	double Y;
 	double Z;
 };
+ 
